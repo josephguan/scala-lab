582ee395cedd2366038d6d842ee04aa55c0ebda3
josephguan <guanxun555@gmail.com>
2017-03-08 22:43:48 +0800

    完善蛋糕模式

--- src/main/scala/gx/types/traits/CakePattern.scala
+++ src/main/scala/gx/types/traits/CakePattern.scala
@@ -1,27 +1,79 @@
 package gx.types.traits
 
+// Cake Pattern
 
-trait Engine {
-  def start(): Unit = {
-    println(s"Engine started. Using $fuelType")
-  }
-
-  def fuelType: String
+/**
+  * 首先，和之前一样，定义元件的接口（如DIYEngine，DIYWheel）
+  */
+trait DIYEngine {
+  def start(): Unit
 }
 
-trait DieselEngine extends Engine {
-  override val fuelType = "Diesel"
+trait DIYWheel {
+  def rotate(): Unit
 }
 
-trait Car {
-  this: Engine =>
+/**
+  * 然后，我们要把元件包在一个Component trait（如EngineComponent和WheelComponent）中，
+  * 并提供一个存取元件的方法（如engine、wheel)
+  */
+trait EngineComponent {
+  val engine: DIYEngine
+}
+
+trait WheelComponent {
+  val wheel: DIYWheel
+}
+
+// 具体实现
+class V6Engine extends DIYEngine {
+  def start(): Unit = println("V6 Engine Start.")
+}
+
+class V8Engine extends DIYEngine {
+  def start(): Unit = println("V8 Engine Start.")
+}
+
+class DunlopWheel extends DIYWheel {
+  def rotate(): Unit = println("Dunlop Wheel Roate")
+}
+
+/**
+  * 在被注入的一方（DIYCar），我们需要使用self-type来声明：
+  * 在构造DIYCar时，必须要提供EngineComponent和WheelComponent。
+  * 同时，在DIYCar内部，我们可以通过engine和wheel两个成员，调用他们提供的接口。
+  */
+abstract class DIYCar {
+  self: EngineComponent with WheelComponent =>
 
   def drive() {
-    start()
+    engine.start()
+    wheel.rotate()
     println("Vroom vroom")
   }
+
+}
+
+/**
+  * 由于我们系统可能包含不止一个元件，建议用Module来定义所有元件
+  * 在Cake Pattern上，我们可以用一个ComponentRegistry trait来把所有的元件指定好。
+  */
+
+trait InMemoryComponentRegistry extends EngineComponent with WheelComponent {
+  override val engine = new V8Engine()
+  override val wheel = new DunlopWheel()
+}
+
+trait XmlComponentRegistry extends EngineComponent with WheelComponent {
+  // read configurations from XML, and construct object by reflect
+  override val engine = new V6Engine()
+  override val wheel = new DunlopWheel()
 }
 
 object CakePatternApp extends App {
-  new Cat().say()
+  val myCar = new DIYCar with InMemoryComponentRegistry
+  myCar.drive()
+
+  val testCar = new DIYCar with XmlComponentRegistry
+  testCar.drive()
 }
--- src/main/scala/gx/types/traits/DependencyInjection.scala
+++ src/main/scala/gx/types/traits/DependencyInjection.scala
@@ -1,19 +1,28 @@
 package gx.types.traits
 
+/**
+  * Dependency Injection & Inversion of Control是Martin Fowler在2004年所提出來的一个概念.
+  * 在IoC概念出现以前，对象依赖的元件（Component）一般是对象建立时就绑定的。 Martin的贡献在于，
+  * IoC把对象对元件的依赖拆出来，变成可以替换的实体。
+  *
+  * IoC主要是通过依赖注入（DI）实现的。
+  *
+  * 在Java中，DI可以有三种形式来实现:
+  * Constructor Injection, Setter Injection, and Interface Injection.
+  *
+  * Scala也可以使用上面三种形式，但是Scala的self-type又提供了一种新的形式
+  *
+  */
 
 trait Engine {
-  def start(): Unit = {
-    println(s"Engine started. Using $fuelType")
-  }
-
-  def fuelType: String
+  def start(): Unit
 }
 
 trait DieselEngine extends Engine {
-  override val fuelType = "Diesel"
+  override def start(): Unit = println("DieselEngine Start.")
 }
 
-trait Car {
+abstract class Car {
   this: Engine =>
 
   def drive() {
