21b195febc34d8478a039cc4c61585bfb303b874
josephguan <guanxun555@gmail.com>
2017-05-09 23:54:03 +0800

    josql test

--- src/main/java/gx/josql/JosqlTest.java
+++ src/main/java/gx/josql/JosqlTest.java
@@ -4,6 +4,7 @@ import org.josql.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Joe on 2017/5/9.
@@ -13,13 +14,21 @@ public class JosqlTest {
     public static void main(String args[]) {
         List<MyObject> obs = getObs();
         Query q = new Query();
-        String sql = "select name, age from gx.josql.MyObject order by age";
+        String sql = "select name, @avg_length from gx.josql.MyObject group by name order by name execute on GROUP_BY_RESULTS avg (:_allobjs, age) AS avg_length";
         try {
             q.parse(sql);
             QueryResults qr = q.execute (obs);
             List res = qr.getResults ();
+            Map gres = qr.getGroupByResults();
+
+            for (Object key : gres.keySet()) {
+
+                System.out.println("Key = " + key);
+
+            }
+
             for (int i = 0; i< res.size(); i++) {
-                System.out.println(res.get(i));
+                System.out.println(gres.get(res.get(i)));
             }
         }catch (Exception e) {
             System.out.print(e);
@@ -33,6 +42,7 @@ public class JosqlTest {
         obs.add(new MyObject("cat", 22));
         obs.add(new MyObject("bas", 42));
         obs.add(new MyObject("foo", 32));
+        obs.add(new MyObject("foo", 30));
         return obs;
 
     }
